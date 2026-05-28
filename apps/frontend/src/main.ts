ce592cd692ef057d7f7a7ba1ef9898e826609659
scarletborder <baishuibeef@gmail.com>
2026-05-28 22:05:06 +0800

    Revert "chore: 游戏强制使用http"
    This reverts commit b53c2060370bf06f368d471fc76f452cf1d7b99b.

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -4,24 +4,18 @@ import { BattleScene } from "./battle-scene";
 import { BattleStartScene, CodexScene, HomeScene, LoadingScene, ResultScene, RoomListScene, RoomLobbyScene, SelectScene, SettingsScene } from "./menu";
 import "./styles.css";
 
-if (window.location.protocol === "https:") {
-  const redirectUrl = new URL(window.location.href);
-  redirectUrl.protocol = "http:";
-  window.location.replace(redirectUrl.toString());
-} else {
-  new Phaser.Game({
-    type: Phaser.AUTO,
-    parent: "game",
-    width: 1280,
-    height: 720,
-    backgroundColor: "#101820",
-    scene: [HomeScene, BattleStartScene, RoomListScene, RoomLobbyScene, SelectScene, LoadingScene, BattleScene, ResultScene, CodexScene, SettingsScene],
-    dom: {
-      createContainer: true,
-    },
-    scale: {
-      mode: Phaser.Scale.FIT,
-      autoCenter: Phaser.Scale.CENTER_BOTH,
-    },
-  });
-}
+new Phaser.Game({
+  type: Phaser.AUTO,
+  parent: "game",
+  width: 1280,
+  height: 720,
+  backgroundColor: "#101820",
+  scene: [HomeScene, BattleStartScene, RoomListScene, RoomLobbyScene, SelectScene, LoadingScene, BattleScene, ResultScene, CodexScene, SettingsScene],
+  dom: {
+    createContainer: true,
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+});
