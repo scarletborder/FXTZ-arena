747130f57e6dfe196f2661470a3152369322d3de
scarletborder <baishuibeef@gmail.com>
2026-05-30 13:09:41 +0800

    chore: 新增分包打包，减小产物体积

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,6 +2,21 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: process.env.VITE_APP_BASE ?? "/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/phaser")) {
+            return "phaser";
+          }
+
+          if (id.includes("node_modules/@dimforge/rapier")) {
+            return "rapier";
+          }
+        },
+      },
+    },
+  },
   server: {
     port: 3000,
   },
