c06f42538a5594b9916a6b87d7a43881a0b4fd40
scarletborder <baishuibeef@gmail.com>
2026-06-02 12:53:36 +0800

    fix(battle): 修正OWN_PROJECTILE_ALPHA常量值为0.2
    - 自己的弹幕减小可见度

--- packages/constants/src/battle/index.ts
+++ packages/constants/src/battle/index.ts
@@ -7,4 +7,4 @@ export * from "./point";
 export const DEFAULT_LIVES = 2;
 export const DEFAULT_BOMBS = 3;
 export const DEFAULT_COST_LIMIT = 10;
-export const OWN_PROJECTILE_ALPHA = 0.4;
+export const OWN_PROJECTILE_ALPHA = 0.2;
