2e53496702a3356fbd58f59a82e0e4e4e7605aef
scarletborder <baishuibeef@gmail.com>
2026-07-01 14:52:23 +0800

    移除公共服务器地址

--- packages/constants/src/sync/link.ts
+++ packages/constants/src/sync/link.ts
@@ -7,16 +7,6 @@ export interface PublicServer {
 
 export const PUBLIC_SERVER: readonly PublicServer[] = [
   {
-    name: "湖北十堰1号",
-    addr: "wss://160.202.238.18:47343/",
-    selfAuth: true,
-  },
-  {
-    name: "湖北十堰1号(desktop专用)",
-    addr: "https://160.202.238.18:47343/wt",
-    selfAuth: true,
-  },
-  {
     name: "local",
     addr: "ws://localhost:22334/",
     selfAuth: true,
